1e4198bc679dc51dfcf9b87e93867233f06e9713
JeongDeoksu <jeongdeoksu@gmail.com>
2026-02-07 19:40:21 +0900

    Ep 3. Adding CRUD actions

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,50 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      flash[:notice] = "Project created successfully!"
+      redirect_to project_path(@project)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    flash[:notice] = "Project updated successfully!"
+    if @project.update(project_params)
+      redirect_to project_path(@project)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+
+    @project.destroy
+    flash[:notice] = "Project deleted"
+    redirect_to projects_path
+  end
+
+  def project_params
+    params.require(:project).permit(:name)
+  end
+end
